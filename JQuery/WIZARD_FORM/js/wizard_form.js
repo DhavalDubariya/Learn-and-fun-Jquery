19860176a7d392c1c113be86dcf0c120c6187601
DhavalDubariya <dhavaldubariya70@gmail.com>
2022-04-18 12:17:59 +0530

    Wizard Form Update April_18

--- JQuery/WIZARD_FORM/js/wizard_form.js
+++ JQuery/WIZARD_FORM/js/wizard_form.js
@@ -1,20 +1,17 @@
 $(document).ready(function () {
-  //tebs
+  $(".update,.cancel").hide();
 
+  //tebs
   $(".teb").hide().first().show();
   $(".button").click(function () {
     $(".teb").hide().eq($(this).index()).show();
   });
+
   //Next Button
   $(".next").click(function () {
     var index = $(".next").index(this);
-    $(".teb")
-      .hide()
-      .eq(index + 1)
-      .show();
+    $(".teb").hide().eq(index + 1).show();
   });
-
-  $(".update").hide();
 
   //Previous Button
   $(".previous").click(function () {
@@ -22,78 +19,71 @@ $(document).ready(function () {
     $(".teb").hide().eq(index).show();
   });
 
-  //cancel Button
-  $(".cancel").hide();
-  // Submit Button
-  $(".cancel").click(function () {
+  //Empty Value Of Input's
+  function EmptyVal() {
     $("input[type=text],textarea").val("");
     $("input[type='radio']:checked,input[type=checkbox]").prop(
       "checked",
       false
     );
-  });
+    $(".teb").hide().first().show();
+  }
 
+
+  // Submit Button
   $(".submit").click(function () {
-    var f_name = $("#f_name").val();
-    var l_name = $("#l_name").val();
-    var gender = $("input[type='radio']:checked").val();
-    var email = $("#email").val();
-    var contact_no = $("#contact_no").val();
-    var dob = $("#dob").val();
-    var sport = $("#sport").val();
-    var about = $("textarea#message").val();
-    var terms = $("input[type='checkbox']:checked").val();
     var index = $("tr").length;
-
+    GetValues();
+    
     $(".before").append(
       `<tr class="table_data" ><td class="new_index" >${index}</td><td class="f_name" > ${f_name} </td><td class="l_name" >${l_name}</td><td class="gender">${gender}</td><td class="email">${email}</td><td class="contact_no" >${contact_no}</td><td class="dob" >${dob}</td><td class="sport" >${sport}</td><td class="about" >${about}</td><td class="terms">${terms}</td><td><button class="edit">Edit</button></td><td><button class="delete">Delete</button></td></tr>`
     );
 
-    $("input[type=text],textarea").val("");
-    $("input[type='radio']:checked,input[type=checkbox]").prop(
-      "checked",
-      false
-    );
-
-    // $(".teb").hide().first().show();
+    EmptyVal();
   });
 
+  //Update Button
   $(".update").click(function () {
-    var f_name = $("#f_name").val();
-    var l_name = $("#l_name").val();
-    var gender = $("input[type='radio']:checked").val();
-    var email = $("#email").val();
-    var contact_no = $("#contact_no").val();
-    var dob = $("#dob").val();
-    var sport = $("#sport").val();
-    var about = $("textarea#message").val();
-    var terms = $("input[type='checkbox']:checked").val();
+    GetValues();
+
     var index = $("tr").length;
 
     index = parseInt($("#index").val());
-    //  alert($("#index").val());
+
     let html = `<td class="new_index" >${index}</td><td class="f_name" > ${f_name} </td><td class="l_name" >${l_name}</td><td class="gender">${gender}</td><td class="email">${email}</td><td class="contact_no" >${contact_no}</td><td class="dob" >${dob}</td><td class="sport" >${sport}</td><td class="about" >${about}</td><td class="terms">${terms}</td><td><button class="edit">Edit</button></td><td><button class="delete">Delete</button></td>`;
     console.log(html);
-    $(".table_data")
-      .eq(parseInt(index - 1))
-      .html(html);
-    $(".submit").text("SUBMIT");
+    $(".table_data").eq(parseInt(index - 1)).html(html);
 
-    $("input[type=text],textarea").val("");
-    $("input[type='radio']:checked,input[type=checkbox]").prop(
-      "checked",
-      false
-    );
+    EmptyVal();
 
-    $(".cancel").hide();
-    $(".update").hide();
+    $(".cancel,.update").hide();
     $(".submit").show();
-  
-
-    // $(".teb").hide().first().show();
   });
+
+  //Get Value Of Input's
+  function GetValues() {
+    f_name = $("#f_name").val();
+    l_name = $("#l_name").val();
+    gender = $("input[type='radio']:checked").val();
+    email = $("#email").val();
+    contact_no = $("#contact_no").val();
+    dob = $("#dob").val();
+    sport = $("#sport").val();
+    about = $("textarea#message").val();
+    terms = $("input[type='checkbox']:checked").val();
+
+    ($("#terms").is(":checked")) ? terms = "set" : terms = "off";
+  }
+
+  //Cancle Button
+  $(".cancel").click(function () {
+    EmptyVal();
+  });
+
 });
 
+
+// Delete Button
 $(document).on("click", ".delete", function () {
   var DeleteIndex = $(".delete").index(this);
   $(".table_data").eq(DeleteIndex).remove();
@@ -102,10 +92,9 @@ $(document).on("click", ".delete", function () {
     $($(this).find("td")[0]).html(i);
   });
 });
-
+// Edit Button
 $(document).on("click", ".edit", function () {
   var EditIndex = $(".edit").index(this);
-
   var new_index = $(".new_index").eq(EditIndex).text();
   var f_name = $(".f_name").eq(EditIndex).text();
   var l_name = $(".l_name").eq(EditIndex).text();
@@ -125,16 +114,13 @@ $(document).on("click", ".edit", function () {
   $("#dob").val(dob);
   $("#sport").val(sport);
   $("textarea#message").val(about);
-  gender != "undefined"
-    ? gender == "male"
-      ? $("#male").prop("checked", true)
-      : $("#female").prop("checked", true)
-    : $("input[type=radio]").prop("checked", false);
 
-  (terms  == "undefined") ?  $("input[type=checkbox]").prop("checked", false) : $("input[type=checkbox]").prop("checked", true);
+  gender != "undefined" ? gender == "male" ? $("#male").prop("checked", true) : $("#female").prop("checked", true) : $("input[type=radio]").prop("checked", false);
+
+  terms == "off" ? $("input[type=checkbox]").prop("checked", false) : $("input[type=checkbox]").prop("checked", true);
 
   $(".submit").hide();
-  $(".cancel").show();
+  $(".cancel,.update").show();
   $(".teb").hide().first().show();
-  $(".update").show();
+
 });
