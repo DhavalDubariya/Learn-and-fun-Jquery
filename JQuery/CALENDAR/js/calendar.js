9cedc73cb0ddedc13179504b2038f1bb10351f22
DhavalDubariya <dhavaldubariya70@gmail.com>
2022-04-22 13:18:52 +0530

    Find Function

--- JQuery/CALENDAR/js/calendar.js
+++ JQuery/CALENDAR/js/calendar.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   var today_date = new Date();
-  
-
+  var bool = false;
   var months = [
     "January",
     "February",
@@ -25,89 +24,84 @@ $(document).ready(function () {
     "Friday",
     "Saturday",
   ];
+  for (let i = 1970; i <= 2070; i++) {
+    $("#select_year").append(`<option value="${i}">${i}</option>`);
+  }
 
-  function calendar() {
+  for (let j = 0; j < 12; j++) {
+    $("#select_month").append(
+      `<option class="click_month" value="${j + 1}">${months[j]}</option>`
+    );
+    // console.log(months[j]);
+  }
 
-    var month = today_date.getMonth();
-    var day = today_date.getDay();
-    var year = today_date.getFullYear();
-    var date = today_date.getDate();
-
-    for(let i=1970;i<=2070;i++)
-    {
-      
-      $(".select_year").append(`<option value="${i}">${i}</option>`);
-
-    }
-
-    for(let j=0;j<12;j++)
-    {
-      $(".select_month").append(`<option value="${j}">${months[j]}</option>`);
-      // console.log(months[j]);
-    }
-
-    for(let k=1;k<32;k++)
-    {
-      $(".select_date").append(`<option value="${k}">${k}</option>`);
-    }
-    // console.log(date,month,year);
+  $("#select_year, #select_month").change(function () {
     
-    var feb;
-    if (month === 1) {
-      if ((year % 100 !== 0 && year % 4 === 0) || year % 400 === 0) {
-        feb = 29;
-      }
-      else
-      {
-        feb = 28;
-      }
-    }
+      $("#days_month").find("option").remove();
+      var days_in_month = new Date($("#select_year").val(),$("#select_month").val(),0).getDate();
+      for (var i = 1; i <= days_in_month; i++) {
+        $("#days_month").append(`<option value="${i}" >${i}</option>`);
+      }                     
+    
+  });
 
-    var firstday = new Date(year,month,1);
-    firstday_date = firstday.getDay();
-    var lastday = new Date(year,month+1,0);
-    var lastday_date = lastday.getDay();    
-    // console.log(firstday_date,lastday_day);
-    // console.log(firstday,lastday);
+  $("#find_button").click(function () {
+    year = $("#select_year").val();
+    month = $("#select_month").val();
+    date = $("#days_month").val();
+
+    today_date.setMonth(month - 1);
+    today_date.setFullYear(year);
+    today_date.setDate(date);
+    
+    bool = true;
+
+    $(".before").empty();
+    calendar(bool);
+  });
+
+  function calendar(flage) {
+    month = today_date.getMonth();
+    day = today_date.getDay();
+    year = today_date.getFullYear();
+    date = today_date.getDate();
+
+    var firstday = new Date(year, month, 1);
+    var firstday_date = firstday.getDay();
+    var days_in_month = new Date(year, month + 1, 0).getDate();
     var current_date = new Date();
-
-    
-
-    var days_in_month = [31, feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     var display = `${months[month]} - ${year} `;
-
     $(".date_month").text(display);
-    count = 0;  
+
+    count = 0;
     count_new = 0;
 
     for (let i = 0; i < 7; i++) {
-        $(".before").append(`<tr>`);
-        for(let j = 0; j < 7; j++)
-        {   
-            count++;
-            if(firstday_date<count)
-            {   
-                ++count_new;
-                if(count_new == date && current_date.getMonth() == month  && current_date.getFullYear() == year)
-                {
-                    $(".before").append(`<td class="today" >${count_new}</td>`);
-                }
-                else if(count_new-1==days_in_month[month])
-                {
-                    return false;
-                }
-                else
-                {
-                    $(".before").append(`<td>${count_new}</td>`);
-                }
-            }
-            else
-            {
-                $(".before").append(`<td></td>`);
-            }
-            
+      $(".before").append(`<tr>`);
+      for (let j = 0; j < 7; j++) {
+        count++;
+        if (firstday_date < count) {
+          ++count_new;
+          if (
+            count_new == date &&
+            current_date.getMonth() == month &&
+            current_date.getFullYear() == year
+          ) {
+            $(".before").append(`<td class="today" >${count_new}</td>`);
+          } else if (flage == true && count_new == date) {
+            $(".before").append(
+              `<td class="today" >${count_new}</td>`
+            );
+          } else if (count_new - 1 == days_in_month) {
+            return false;
+          } else {
+            $(".before").append(`<td>${count_new}</td>`);
+          }
+        } else {
+          $(".before").append(`<td></td>`);
         }
-        $(".before").append(`</tr>`);
+      }
+      $(".before").append(`</tr>`);
     }
   }
 
@@ -119,17 +113,15 @@ $(document).ready(function () {
     calendar();
   });
 
-
   $("#next").click(function () {
     today_date.setMonth(today_date.getMonth() + 1);
     $(".before").empty();
     calendar();
   });
 
-  $("#today_button").click(function ()  {
+  $("#today_button").click(function () {
     today_date = new Date();
     $(".before").empty();
     calendar();
   });
-
 });
