c77454767b3c9c0a10e667bd45904d7b54076473
DhavalDubariya <dhavaldubariya70@gmail.com>
2022-04-25 16:17:41 +0530

    calc

--- JQuery/CALCULATOR/js/test.js
+++ JQuery/CALCULATOR/js/test.js
@@ -9,23 +9,22 @@ $(document).ready(function () {
       if (value_of_btn == "esc") {
         $("#input_cal").val("");
       } else if (value_of_btn == "back") {
-          if(!(parseInt(parseFloat(value_of_input_btn)) == 0 && value_of_input_btn.length == 1))
-          $("#input_cal").val(value_of_input_btn.slice(0, value_of_input_btn.length - 1));
+          if(
+          !(
+            parseInt(parseFloat(value_of_input_btn)) == 0 &&
+            value_of_input_btn.length == 1
+          )
+        )
+          $("#input_cal").val(
+            value_of_input_btn.slice(0, value_of_input_btn.length - 1)
+          );
         if (value.length == 1) $("#input_cal").val("0");
-      }
-      else if(value_of_btn == "root")
-      {
-        $("#input_cal").val(Math.sqrt(value_of_input_btn));
-        
-      }
+      } 
       else {
         $("#input_cal").val(value_of_input_btn + value_of_btn);
       }
     });
-
-    $(document).keydown(function (){
-        $("#input_cal").focus();
-    });
+  
     
     //Esc Button
     $("#input_cal").keydown(function (event) {
@@ -41,9 +40,8 @@ $(document).ready(function () {
   
     $(".btn_eq").click(function () {
       var value_of_input_btn = $("#input_cal").val();
-      var ans = eval(value_of_input_btn);
+      var ans = math.eval(value_of_input_btn);
       $("#input_cal").val(ans);
     });
-
   });
   
