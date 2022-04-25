785b328e1f83453f9134e5d4d9d7df4f23d1a1b4
DhavalDubariya <dhavaldubariya70@gmail.com>
2022-04-25 20:28:22 +0530

    Calculator

--- JQuery/CALCULATOR/js/calculator.js
+++ JQuery/CALCULATOR/js/calculator.js
@@ -1,49 +1,96 @@
 $(document).ready(function () {
-    $(".input_btn").click(function () {
-      var index = $(".input_btn").index(this);
-  
-      var value_of_btn = $(".input_btn").eq(index).val();
-  
-      var value_of_input_btn = $("#input_cal").val();
-  
-      if (value_of_btn == "esc") {
-        $("#input_cal").val("");
-      } else if (value_of_btn == "back") {
-          if(!(parseInt(parseFloat(value_of_input_btn)) == 0 && value_of_input_btn.length == 1))
-          $("#input_cal").val(value_of_input_btn.slice(0, value_of_input_btn.length - 1));
-        if (value.length == 1) $("#input_cal").val("0");
-      }
-      else if(value_of_btn == "root")
-      {
-        $("#input_cal").val(Math.sqrt(value_of_input_btn));
-        
-      }
-      else {
-        $("#input_cal").val(value_of_input_btn + value_of_btn);
-      }
-    });
+ 
+  $("body").click(function () {
+      $(".input_cal").focus();
+  });
 
-    $(document).keydown(function (){
-        $("#input_cal").focus();
-    });
-    
-    //Esc Button
-    $("#input_cal").keydown(function (event) {
-      if (event.keyCode === 27) {
-        $("#input_cal").val("");  
-      }
-      if (event.keyCode === 13) {
-        var value_of_input_btn = $("#input_cal").val();
-        var ans = eval(value_of_input_btn);
-        $("#input_cal").val(ans);
-      }
-    });
+  $(".input_btn").click(function () {
+    var index = $(".input_btn").index(this);
+
+    var value_of_btn = $(".input_btn").eq(index).val();
+
+    var value_of_input_btn = $("#input_cal").val();
+    if (value_of_btn == "esc") {
+      $("#input_cal").val("");
+    } 
+    else if (value_of_btn == "back") {
+      if (
+        !(
+          parseInt(parseFloat(value_of_input_btn)) == 0 &&
+          value_of_input_btn.length == 1
+        )
+      )
+        $("#input_cal").val(
+          value_of_input_btn.slice(0, value_of_input_btn.length - 1)
+        );
+      if (value.length == 1) $("#input_cal").val("0");
+    }else {
+      $("#input_cal").val(value_of_input_btn + value_of_btn);
+    }
+  });
+
+  //Esc Button
+  $("#input_cal").keydown(function (event) {
+    if (event.keyCode === 27) {
+      $("#input_cal").val("");
+    }
+    if (event.keyCode === 13) {
+      try
+      {
+      var text = $("#input_cal").val();
+      var multiply = text.replace(/x/gi, "*");
+      var division = multiply.replace(/÷/gi, "/");
+      var squre = division.replace(/²/gi, "**2 ");
   
-    $(".btn_eq").click(function () {
-      var value_of_input_btn = $("#input_cal").val();
+      value_of_input_btn = squre.replace(/√/gi,"**0.5 ");
+  
+      // console.log(value_of_input_btn);
+      if(ans == "Infinity") {throw "Division by zero is undefined";}
       var ans = eval(value_of_input_btn);
       $("#input_cal").val(ans);
-    });
-
+      }
+      catch (e) 
+      {
+        if(e instanceof SyntaxError)
+        {
+          $("#input_cal").val("Malformed Expression");
+        }
+        else
+        {
+          $("#input_cal").val(e);
+        }
+      }
+      
+    }
   });
-  
+
+  $(".btn_eq").click(function () {
+    try
+    {
+    var text = $("#input_cal").val();
+    var multiply = text.replace(/x/gi, "*");
+    var division = multiply.replace(/÷/gi, "/");
+    var squre = division.replace(/²/gi, "**2 ");
+
+    value_of_input_btn = squre.replace(/√/gi,"**0.5 ");
+
+    // console.log(value_of_input_btn);
+    if(ans == "Infinity") {throw "Division by zero is undefined";}
+    var ans = eval(value_of_input_btn);
+    $("#input_cal").val(ans);
+    }
+    catch (e) 
+    {
+      if(e instanceof SyntaxError)
+      {
+        $("#input_cal").val("Malformed Expression");
+      }
+      else
+      {
+        $("#input_cal").val(e);
+      }
+    }
+    
+    
+  });
+});
