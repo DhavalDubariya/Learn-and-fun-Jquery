77fbe1b756efb723dca7de9183b5c70066c71f6a
DhavalDubariya <dhavaldubariya70@gmail.com>
2022-05-11 16:05:51 +0530

    Dynamic Menu Creation Task-Done

---
+++ JQuery/DYNAMIC_MENU_CREATION/ReviewTask-2 - DynamicMenu/html/js/custom.js
@@ -0,0 +1,157 @@
+$(document).ready(function(){
+    var idIncre = 1;
+
+    $("#update").hide();
+
+    
+    $("#add").click(function(){
+        var inputVal = $('#inputValue').val();
+        if($("#all-dropdown").find('select').length > 1) {
+            var targetSel = $("#all-dropdown").find('select').last();
+            if(targetSel.val()=='none')
+            {
+                var targetEle = $(`#menu-list`).find(`li[data-id=${targetSel.prev().val()}]`);
+                targetEle.find('>ul').append(`
+                    <li data-id='${idIncre}' data-text='${inputVal}'>
+                        <p>${inputVal}</p>
+                        <button class="edit_btn" >Edit</button>
+                        <button class="remove_btn" >Remove</button>
+                    </li>`);
+                $("#all-dropdown").find('select').last().append(`<option data-id='${idIncre}' value='${idIncre}'>${inputVal}</option>`);               
+            } 
+            else 
+            {
+                var targetEle = $(`#menu-list`).find(`li[data-id=${targetSel.val()}]`);
+                $("#all-dropdown").append(`<select class="select_menu"><option value='none'>Select Child</option><option data-id='${idIncre}' value='${idIncre}'>${inputVal}</option></select>`);
+                targetEle.append(`
+                <ul>
+                    <li data-id='${idIncre}' data-text='${inputVal}'>
+                        <p>${inputVal}</p>                        
+                        <button class="edit_btn" >Edit</button>
+                        <button class="remove_btn" >Remove</button>
+                    </li>
+                </ul>`);
+            }
+            
+        }
+        else 
+        {
+            if($('.parent-dropdown').val() == 'none')
+            {
+                $('#menu-list>ul').append(`
+                    <li data-id='${idIncre}' data-text='${inputVal}'>
+                        <p>${inputVal}</p>                        
+                        <button class="edit_btn" >Edit</button>
+                        <button class="remove_btn" >Remove</button>
+                    </li>`);
+                $('.parent-dropdown').append(`<option data-id='${idIncre}' value='${idIncre}'>${inputVal}</option>`);
+            } 
+            else 
+            {
+                var targetEle = $(`#menu-list`).find(`li[data-id=${$('.parent-dropdown').val()}]`);
+                if(targetEle.find('ul').length>0)
+                {
+                    targetEle.find('>ul').append(`
+                        <li data-id='${idIncre}' data-text='${inputVal}'>
+                            <p>${inputVal}</p>                            
+                            <button class="edit_btn" >Edit</button>
+                            <button class="remove_btn" >Remove</button>
+                        </li>`);
+                    $("#all-dropdown").find('select').last().append(`<option data-id='${idIncre}' value='${idIncre}'>${inputVal}</option>`);
+                } 
+                else
+                {
+                    $("#all-dropdown").append(`<select class="select_menu"><option value='none'>Select Child</option><option data-id='${idIncre}' value='${idIncre}'>${inputVal}</option></select>`);
+                    targetEle.append(`
+                    <ul>
+                        <li data-id='${idIncre}' data-text='${inputVal}'>   
+                            <p>${inputVal}</p>
+                            <button class="edit_btn" >Edit</button>
+                            <button class="remove_btn" >Remove</button>
+                        </li>
+                    </ul>`);
+                }
+            }
+        }
+        // console.log($('.parent-dropdown').val())
+        
+        
+        $('#inputValue').val("");
+        idIncre++;
+    });
+   
+    $(document).on('change','select',function(){
+        $(this).nextAll().remove();
+        if($(this).val() != 'none')
+        {
+            var targetEle = $(`#menu-list`).find(`li[data-id=${$(this).val()}]`);
+            if(targetEle.find('ul').length>0)
+            {
+                $("#all-dropdown").append(`<select class="select_menu"><option value='none'>Select Child</option></select>`);
+
+                for(i=0;i<targetEle.find('ul:first').children().length;i++)
+                {
+                    var dataId = targetEle.find('ul:first').children().eq(i).attr('data-id')
+                    var dataText = targetEle.find('ul:first').children().eq(i).attr('data-text')
+                    $("#all-dropdown").find('select').last().append(`<option data-id='${idIncre}' value='${dataId}'>${dataText}</option>`);
+                }
+            }
+        }
+    });
+
+    $(document).on('click','.edit_btn',function(){
+        var edit_index = $('.edit_btn').index(this);
+        var edit_data = $('.edit_btn').eq(edit_index).parent("li").attr('data-id');
+        var edit_text = $('.edit_btn').eq(edit_index).parent("li").attr('data-text');        
+        $("#inputValue").val(edit_text);
+        
+        
+        $("#update").click(function(){
+            var inputVal = $('#inputValue').val();
+            $(`#menu-list`).find(`li[data-id=${edit_data}]`).find("p").text(inputVal);
+            $(`#menu-list`).find(`li[data-id=${edit_data}]`).attr("data-text",inputVal);
+
+            $("#all-dropdown").find('select').find(`option[data-id=${edit_data}]`).text(inputVal);            
+            $(".edit_btn,.remove_btn").attr("disabled",false);
+        });
+
+        $("#add").hide();
+        $("#update").show();
+        $(".edit_btn,.remove_btn").attr("disabled",true);
+        console.log(edit_data,edit_text)
+    });
+
+
+
+    $(document).on('click','.remove_btn',function(){
+        var remove_index = $('.remove_btn').index(this);
+        var remove_data = $('.remove_btn').eq(remove_index).parent("li").attr('data-id');
+        var length_li = $(this).parents("ul:first").find("li").length;
+        var select_index = $("#all-dropdown").find('select').find(`option[data-id=${remove_data}]`).parent("select").index();
+
+        if(length_li==1)
+        {
+            if(select_index!=0)
+            {
+                $(this).closest("li").parent("ul").remove()
+                $("#all-dropdown").find('select').find(`option[data-id=${remove_data}]`).parent("select").remove();
+            }
+            else
+            {
+                $(this).closest("li").remove()
+                $("#all-dropdown").find('select').find(`option[data-id=${remove_data}]`).remove();    
+            }
+        }
+        else
+        {
+            $(this).closest("li").remove()
+            $("#all-dropdown").find('select').find(`option[data-id=${remove_data}]`).remove();
+        }
+
+        $("select").trigger('change');
+        
+        
+        
+    });
+
+});
--- JQuery/DYNAMIC_MENU_CREATION_1/js/test2.js
+++ JQuery/DYNAMIC_MENU_CREATION_1/js/test2.js
@@ -1,99 +1,100 @@
+ $(document).ready(function () {
+  var select_menu;
 
-$(document).ready(function()
-{
-
-    var select_menu;
-    
-    $(".add_btn").on("click",function(){
-        var input_menu = $(".input_menu").val();
-        var select_menu_st = $(".select_menu").val();
-        if ($("select").length == 1) {
-            select_menu = $("select").eq(0).val();
+  $(".add_btn").on("click", function () {
+    var input_menu = $(".input_menu").val();
+    var select_menu_st = $(".select_menu").val();
+    if ($("select").length == 1) 
+    {
+      select_menu = $("select").eq(0).val();
+    } 
+    else 
+    {
+      for (i = 0; i < $("select").length; i++) {
+        if ($("select").eq(0).val() == "none") 
+        {
+          select_menu = $("select").eq(0).val();
+          break;
+        } 
+        else if ($("select").eq(i).val() == "none") 
+        {
+          select_menu = $("select")
+            .eq(i - 1)
+            .val();
+          break;
         } 
         else 
         {
-            for (i = 0; i < $("select").length; i++) {
-                if ($("select").eq(0).val() == "none") {
-                    select_menu = $("select").eq(0).val();
-                    break;
-                }
-                else if ($("select").eq(i).val() == "none") {
-                    select_menu = $("select").eq(i - 1).val();
-                    break;
-                }
-                else {
-                    select_menu = $("select").eq($("select").length - 1).val();
-                }
-            }
+          select_menu = $("select")
+            .eq($("select").length - 1)
+            .val();
         }
-            
-        if(select_menu!="none")
-        {   
+      }
+    }
 
-            if ($(`[data='${select_menu}_child']`).length == 0) {
+    if (select_menu != "none") {
+      if ($(`[data='${select_menu}_child']`).length == 0) {
+        $(".right")
+          .find(`[data='${select_menu}']`)
+          .append(
+            `<ul data="${select_menu}_length" class="${select_menu}_length" ></ul>`
+          );
 
-                $(".right").find(`[data='${select_menu}']`).append(`<ul data="${select_menu}_length" class="${select_menu}_length" ></ul>`)
-
-                $(".childe_select").append(`
+        $(".childe_select").append(`
                     <div class="delete_rm" >    
                         <select data="${select_menu}_child" class="${select_menu}_child select_menu_child">
                             <option class="parent_child" value="none">none</option>
                         </select>
                     </div>
                 `);
-            }
+      }
 
-            $(`[data='${select_menu}_child']`).append(`<option class="parent_child" value="${input_menu}">${input_menu}</option>`);
+      $(`[data='${select_menu}_child']`).append(
+        `<option class="parent_child" value="${input_menu}">${input_menu}</option>`
+      );
 
-            $(".right").find(`[data='${select_menu}']`).find(`[data='${select_menu}_length']`).append(`
+      $(".right")
+        .find(`[data='${select_menu}']`)
+        .find(`[data='${select_menu}_length']`).append(`
                 <li data="${input_menu}" >
                     <p>${input_menu}</p>
                     <button >Edit</button>
                     <button >Remove</button>
                 </li>
             `);
-
-        }
-        else
-        {
-            $("ul.none").append(`
+    } else {
+      $("ul.none").append(`
             <li data="${input_menu}" >
                 <p>${input_menu} </p>
                 <button >Edit</button>
                 <button >Remove</button>
             </li>`);
-            $(".select_menu").append(`<option value="${input_menu}">${input_menu}</option>`);
-        }
-    });
-
+      $(".select_menu").append(
+        `<option value="${input_menu}">${input_menu}</option>`
+      );
+    }
+  });
 });
 
-   
-$(document).on('change', 'select', function () {
-        var select_index = $("select").index(this);
-        var select_index_val = $("select").eq(select_index).val();
+$(document).on("change", "select", function () {
+  var select_index = $("select").index(this);
+  var select_index_val = $("select").eq(select_index).val();
 
-        if (select_index_val == 'none') {
-             $("select").hide();
-             $("select").val("none");
-            for(let i = 0; i <= select_index; i++) 
-            {
-                $("select").eq(i).show();
-            }
-        } 
-        else 
-        {   
-            $("select").hide();
-            for(let i = 0; i <= select_index; i++) {
-                $("select").eq(i).show();
-            }
-            for(let i=0;i<$("select").length;i++)
-            {
-                if($("select").eq(i).attr("data") == select_index_val+"_child")
-                {
-                    $("select").eq(i).show();
-                }       
-            }
-        }
-        console.log("index : ", select_index_val+"_child");
+  if (select_index_val != "none") {
+    $("select").hide();
+    for (let i = 0; i <= select_index; i++) {
+      $("select").eq(i).show();
+    }
+    for (let i = 0; i < $("select").length; i++) {
+      if ($("select").eq(i).attr("data") == select_index_val + "_child") {
+        $("select").eq(i).show();
+      }
+    }
+  } else {
+    $("select").hide();
+    $("select").val("none");
+    for (let i = 0; i <= select_index; i++) {
+      $("select").eq(i).show();
+    }
+  }
 });
